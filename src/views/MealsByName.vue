e1b8048c178ea31f36c522b752335b30c5e3f58e
thuanphatt <zelorntp@gmail.com>
2023-03-20 23:01:56 +0700

    install vuex

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -2,10 +2,26 @@
   <div class="p-8">
     <input
       type="text"
+      :v-model="keyword"
       class="rounded border-2 border-gray-200 w-full"
       placeholder="Search for meals"
+      @change="searchMeals"
     />
   </div>
+  <div>
+    <pre>{{ meals }}</pre>
+  </div>
 </template>
-<script setup></script>
+<script setup>
+import { computed } from "@vue/reactivity";
+import { ref } from "vue";
+import store from "../store";
+
+const keyword = ref("");
+const meals = computed(() => store.state.searchedMeals);
+
+const searchMeals = () => {
+  store.dispatch("searchMeals", keyword.value);
+};
+</script>
 <style lang=""></style>
